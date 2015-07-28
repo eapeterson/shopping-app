0872e715cf3761268474d8de05b2231f8c59ed51
eapeterson <elizabeth.peterson1229@gmail.com>
2015-07-28 00:01:51 -0400

    Final commit

--- js/app.js
+++ js/app.js
@@ -1,25 +1,61 @@
 $(document).ready(function() {
-	$('.container').on('click', '#add-items', function(){
-		var text = $('#add-box').val();
-		if(text.length){
-			$('<li />', {html:text}).prependTo('ul.buyitems');
+	var activeList = '#list-grocery';
+	var activeTitle = '.grocery';
+	$('.container').on('click', '#add-items', addItemFunc);
+	$('#add-box').on('keydown', function (event) {
+		if(event.which == 13) {
+			addItemFunc(event);
 		}
-		$('ul').show();
 	})
 
+	function addItemFunc (event) {
+		var text = $('#add-box').val();
+		$('#add-box').val('');
+		if(text.length){
+			$('<li />', {html:text}).prependTo('ul.' + activeList);
+		}	
+	}
+/*This is not working*/	
+	$('.container').on('click', 'ul.' + activeList + 'li.items', function(event){
+		$('ul.' + activeList + 'li.items').toggleClass('.purchased');
+	})
+/*This is not working*/
 	$('.container').on('click', '#reset-items', function(event){
-		$('ul').hide();
-
+		console.log('removing selected');
+		$('ul.' + activeList + 'li.purchased').remove();
 	})
 
 	$('.container').on('click', '#saved', function(){
 		$('#nextlist').show();
-		$('#thirdlist').show();
 	})
 
-	$('container').on('click', '#nextlist', function() {
-		
+	$('.container').on('click', '#nextlist', function(event) {
+		event.preventDefault();
+			if(activeList == '#list-grocery'){
+			activeList = '#list-todo';
+			} else if(activeList == '#list-todo') {
+			activeList = '#list-hardware';
+			} else if(activeList == '#list-hardware') {
+			activeList = '#list-grocery';
+			}
+			if(activeTitle == '.grocery'){
+			activeTitle = '.todo';
+			} else if(activeTitle == '.todo') {
+			activeTitle = '.hardware';
+			} else if(activeTitle == '.hardware') {
+			activeTitle = '.grocery';
+			}
+			showActiveList();
+			showActiveTitle();
 	})
 
-	
-})
+	function showActiveList () {
+		$('.list').hide();
+		$('ul.' + activeList).show();
+	}
+
+	function showActiveTitle () {
+		$('h2').hide();
+		$('h2' + activeTitle).show();
+	}
+});
